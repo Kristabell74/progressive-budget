d757d0c83b06011166acbf67a8731a73f1efe38e
Kristabell Johnson <bell77iam@gmail.com>
2021-04-03 19:42:44 -0600

    server.js modification for mongodb

--- server.js
+++ server.js
@@ -16,19 +16,22 @@ app.use(express.json());
 
 
 
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false,
-//   useCreateIndex: true
-// });
+mongoose.connect(
+  process.env.MONGOD_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }
+);
 
-const db = require('./config/keys').mongoURI;
-console.log(db, 'this is the db')
-mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("connected to mongodb succesfully"))
-  .catch(err => console.log(err))
+// const db = require('./config/keys').mongoURI;
+// console.log(db, 'this is the db')
+// mongoose
+//   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log("connected to mongodb succesfully"))
+//   .catch(err => console.log(err))
 
 
 app.use(express.static(path.join(__dirname, 'public')));
